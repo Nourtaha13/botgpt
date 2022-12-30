836e847108fe10fb3bb37349f454ab26d259e28e
Nour Eldin <Nourraaft@gmail.com>
2022-12-30 16:08:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,30 +4,30 @@ import cors from "cors";
 import { botTele } from "./bot.js";
 import { openAiDate } from "./openai.js";
 dotenv.config();
-const PORT = process.env.PORT_SERVER || 5000;
+const PORT = process.env.PORT || 8080;
 botTele();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//    res.status(200).json({
-//       message: "Hello from Chat gpt",
-//    });
-// });
+app.get("/", (req, res) => {
+   res.status(200).json({
+      message: "Hello from Chat gpt",
+   });
+});
 
-// app.post("/", async (req, res) => {
-//    try {
-//       const prompt = await req.body.prompt;
-//       res.status(200).json({
-//          bot: await openAiDate(prompt),
-//       });
-//    } catch (error) {
-//       return res.status(500).json({ message: error.message });
-//    }
-// });
+app.post("/", async (req, res) => {
+   try {
+      const prompt = await req.body.prompt;
+      res.status(200).json({
+         bot: await openAiDate(prompt),
+      });
+   } catch (error) {
+      return res.status(500).json({ message: error.message });
+   }
+});
 
-// app.listen(PORT, () =>
-//    console.log(`Server is running on http://localhost:${PORT}`)
-// );
+app.listen(PORT, () =>
+   console.log(`Server is running on http://localhost:${PORT}`)
+);
