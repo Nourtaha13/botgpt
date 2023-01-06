55c52443456c3b12aeee1f17fa1a815247fa4066
Nour Eldin <Nourraaft@gmail.com>
2023-01-06 14:49:05 +0200

    update

--- mongoose.js
+++ mongoose.js
@@ -45,6 +45,24 @@ const SchemaChat = mongoose.Schema(
    }
 );
 
+const SchemaViews = mongoose.Schema({
+   _id: {
+      type: String,
+      default: Math.floor(Math.random() * 1000000 + 1),
+   },
+   page: {
+      type: String,
+      default: "home"
+   },
+   views: {
+      type: Number,
+      default: 0
+   }
+}, {
+   timestamps: true,
+})
+
 export const Model = mongoose.model("User", Schema);
 export const GroupModel = mongoose.model("group", Schema);
+export const ViewsModel = mongoose.model("views", SchemaViews);
 
